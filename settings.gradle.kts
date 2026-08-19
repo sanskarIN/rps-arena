e00405de4862aa339cf4e4ce2ec0c2efeff60189
Sanskar <sanskarin@outlook.in>
2026-08-19 08:10:37 +0530

    build: configure project repositories

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "rps-arena"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
