75dd8f7080f7faf3defff6f2d3be4228258f590a
Sanskar <sanskarin@outlook.in>
2026-08-19 08:12:26 +0530

    build: expose platform plugin aliases

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidMultiplatformLibrary) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.composeCompiler) apply false
 }
