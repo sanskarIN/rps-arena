51891fe33ac8dd4fe2cc7343eb6a51811646ec43
Sanskar <sanskarin@outlook.in>
2026-08-19 08:18:50 +0530

    fix: import responsive row weight modifier

--- shared/src/commonMain/kotlin/dev/sanskar/rpsarena/ui/App.kt
+++ shared/src/commonMain/kotlin/dev/sanskar/rpsarena/ui/App.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
