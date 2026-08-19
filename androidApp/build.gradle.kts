b96f406bdab2d5de6701ca28a2cc771d66a7e9f0
Sanskar <sanskarin@outlook.in>
2026-08-19 14:38:15 +0530

    build: bump Android application to version 1.1.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "in.sanskar.rpsarena"
         minSdk = libs.versions.androidMinSdk.get().toInt()
         targetSdk = libs.versions.androidTargetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildFeatures {
