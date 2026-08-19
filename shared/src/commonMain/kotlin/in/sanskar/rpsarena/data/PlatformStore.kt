1b8b700ee47aa94d4dab07af0a38e37e5f9e60b8
Sanskar <sanskarin@outlook.in>
2026-08-19 08:37:41 +0530

    refactor: make persistence store testable

--- shared/src/commonMain/kotlin/in/sanskar/rpsarena/data/PlatformStore.kt
+++ shared/src/commonMain/kotlin/in/sanskar/rpsarena/data/PlatformStore.kt
@@ -1,7 +1,12 @@
 package in.sanskar.rpsarena.data
 
-expect object PlatformStore {
-    fun initialize(platformContext: Any? = null)
+interface KeyValueStore {
     fun getString(key: String, defaultValue: String = ""): String
     fun putString(key: String, value: String)
+}
+
+expect object PlatformStore : KeyValueStore {
+    fun initialize(platformContext: Any? = null)
+    override fun getString(key: String, defaultValue: String): String
+    override fun putString(key: String, value: String)
 }
