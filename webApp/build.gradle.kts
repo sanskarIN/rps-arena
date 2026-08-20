7340a9c1c48b0af69248c9300b87c2c6d5d9aa59
Sanskar <sanskarin@outlook.in>
2026-08-20 06:13:59 +0530

    feat(web): add browser application module

---
+++ webApp/build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        wasmJsMain.dependencies {
+            implementation(project(":shared"))
+            implementation(compose.runtime)
+            implementation(compose.ui)
+        }
+    }
+}
